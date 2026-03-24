8a2f98ee09adfaed6000d58bcd62e174aad83c84
goodmanagement-s <goodmanagement29@gmail.com>
2026-03-24 02:14:19 +0000

    21 Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ app.use(express.json());
 const upload = multer({ dest: "uploads/" });
 
 // ✅ CONNECT TO MONGODB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
 // Schema
 const applicationSchema = new mongoose.Schema({
